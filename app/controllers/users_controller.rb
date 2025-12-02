f20c7e1d4231fc0afbe0bcf9c2cf7358ce304244
Pierre Gozard <pierre.gozard03@gmail.com>
2025-12-02 16:12:06 +0100

    friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only: :show
   def index
     @users = User.all
   end
+
   def show
-    @user = User.find(params[:id])
+    @friends = Friend.where(user_main_id: @user.id)
   end
 
+  def friends
+  @friends = Friend.all
+  end
+
+
+private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
