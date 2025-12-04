4370af3a786b2b31f8adf0e49ead4493ca43afb9
aurelienlefrere <aurelienlefrere@gmail.com>
2025-12-04 17:22:51 +0100

    index_friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,6 +4,5 @@ class FriendsController < ApplicationController
   def index
     friends_as_main = Friend.where(user_main_id: current_user.id).map(&:user_friend)
     friends_as_friend = Friend.where(user_friend_id: current_user.id).map(&:user_main)
-    @friends = friends_as_main + friends_as_friend
   end
 end
