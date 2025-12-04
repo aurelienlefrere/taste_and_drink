3e925fb832f1ab5ed1ba46b648dea1742e245842
Pierre Gozard <pierre.gozard03@gmail.com>
2025-12-04 17:10:04 +0100

    form OK

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -18,18 +18,23 @@ class MealsController < ApplicationController
     @meal = Meal.new(meal_params)
     @meal.user = current_user
     if @meal.save
+      @guest_ids = guest_params[:guest_ids].compact_blank
+      @guest_ids.each do |guest|
+        Guest.create(user_id: guest.to_i, meal: @meal)
+      end
     redirect_to root_path
     else
     render :new, status: :unprocessable_entity
     end
   end
 
-  # def
-  #   params[:meal][:guest_ids]
-  # end
+
 
 private
   def meal_params
-    params.require(:meal).permit(:dish_name, :friends)
+    params.require(:meal).permit(:dish_name)
+  end
+  def guest_params
+    params.require(:meal).permit(guest_ids: [])
   end
 end
