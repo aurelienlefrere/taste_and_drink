ab7585667d43bf581d13568564b271d7fce04612
aurelienlefrere <aurelienlefrere@gmail.com>
2025-12-05 17:01:16 +0100

    operationnel

--- app/controllers/mealrecommendations_controller.rb
+++ app/controllers/mealrecommendations_controller.rb
@@ -1,21 +1,22 @@
 class MealrecommendationsController < ApplicationController
 
-  def new
-  @meal_recommendations = MealRecommendation.new
-  end
+  # def new
+  # @meal_recommendations = MealRecommendation.new
+  # end
 
 
 
 
 
 
-  drinks = Drink.nearest_neighbors(:embedding, embedding.vectors, distance: "euclidean").first(2)
-  instructions = system_prompt
-  instructions += drinks.map { |drink| drink_prompt(drink) }.join("\n\n")
-  if @chat.with_instructions(instructions).ask(params[:title][:category])
-    redirect_to chat_path(@chat)
-  else
-    @message = @chat.messages.last
-    render "chats/show", status: :unprocessable_entity
-  end
+  # drinks = Drink.nearest_neighbors(:embedding, embedding.vectors, distance: "euclidean").first(2)
+  # instructions = system_prompt
+  # instructions += drinks.map { |drink| drink_prompt(drink) }.join("\n\n")
+  # if @chat.with_instructions(instructions).ask(params[:title][:category])
+  #   redirect_to chat_path(@chat)
+  # else
+  #   @message = @chat.messages.last
+  #   render "chats/show", status: :unprocessable_entity
+  # end
+  # end
 end
